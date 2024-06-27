8063c054582d8b37058d2aee1568dba99772d1be
Andrejs Popovs <andrejs.popovs@sapiens.com>
2024-06-27 18:07:39 +0300

    changed client to anthropic

--- lizka.py
+++ lizka.py
@@ -1,13 +1,15 @@
 import base64
 import requests
 import os
+import anthropic
 from io import BytesIO
 from openai import OpenAI
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
+anthropicClient = anthropic.Anthropic()
 openAiKey = os.environ['OPENAI_API_KEY']
-client = OpenAI(api_key=openAiKey)
+openAiClient = OpenAI(api_key=openAiKey)
 telegramBotToken = os.environ['TELEGRAM_TOKEN']
 messagesDict = dict()
 
@@ -18,7 +20,7 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         user_text = update.message.text
         if 'рисуй' in user_text or 'draw' in user_text or 'zīmē' in user_text:
-            response = client.images.generate(model='dall-e-3', prompt=user_text, size='1024x1024', quality='standard', n=1)
+            response = openAiClient.images.generate(model='dall-e-3', prompt=user_text, size='1024x1024', quality='standard', n=1)
             await context.bot.send_message(chat_id=chat_id, text=response.data[0].revised_prompt)
             image_url = response.data[0].url
             image_response = requests.get(image_url)
@@ -26,8 +28,13 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_photo(chat_id=chat_id, photo=(BytesIO(image_response.content)))
         else:
             messages.append({'role': 'user', 'content': user_text})
-            response = client.chat.completions.create(model='gpt-4o', messages=messages, temperature=0)
-            response_text = response.choices[0].message.content
+            print(messages)
+            # response = openAiClient.chat.completions.create(model='gpt-4o', messages=messages, temperature=0.0, max_tokens=1000)
+            # response_text = response.choices[0].message.content
+            response = anthropicClient.messages.create(model="claude-3-opus-20240229", max_tokens=1000, temperature=0.0, messages=messages)
+            print(response)
+            response_text = response.content[0].text
+            print(response_text)
             messages.append({'role': 'assistant', 'content': response_text})
             await context.bot.send_message(chat_id=chat_id, text=response_text)
     except Exception as error:
@@ -44,7 +51,7 @@ async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         messages = get_messages(chat_id)
         messages.append({'role': 'user', 'content': [{'type': 'text', 'text': update.message.caption},
                                                  {'type': 'image_url', 'image_url': {'url': f'data:image/jpeg;base64,{downloaded_file}'}}]})
-        response = client.chat.completions.create(model='gpt-4o', messages=messages, temperature=0)
+        response = openAiClient.chat.completions.create(model='gpt-4o', messages=messages, temperature=0)
         response_text = response.choices[0].message.content
         messages.append({'role': 'assistant', 'content': response_text})
         await context.bot.send_message(chat_id, response_text)
